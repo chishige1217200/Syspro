e9f852ee1433b6318935aff4e0d4b0d7d0c647c6
chishige1217200 <daichicchi01@gmail.com>
2020-10-05 16:41:43 +0900

    [add] practice1-2.s

---
+++ 第1回/practice1-2.s
@@ -0,0 +1,13 @@
+        .text                   # textセグメントに配置する指定
+        .align 2                # 4バイト境界に配置する指定
+
+main:                           # main(ここから実行開始)
+        move    $a0,$zero       # $a0 : SUM = 0
+        li      $v1,1           # $v1 : COUNT = 1
+loop:
+        addu    $a0,$a0,$v1     # SUM += COUNT
+        addu    $v1,$v1,1       # COUNT++
+      # slt     $v0,$v1,6       # $v0 = COUNT < 6 ? 1 : 0
+        bnez    $v0,loop        # if ($v0 != 0) goto loop
+        move    $t0,$a0         # $t0 = SUM
+        j       $ra             # return
