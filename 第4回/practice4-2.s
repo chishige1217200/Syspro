88cc9ef58e0d0bb46e2a8a15f8e8d9a3e91faeb1
chishige1217200 <daichicchi01@gmail.com>
2020-10-26 16:39:18 +0900

    [fix] Bad Exception 7

--- 第4回/practice4-2.s
+++ 第4回/practice4-2.s
@@ -9,10 +9,14 @@ main:
 
         li      $a0, 10                 # $a0に10を代入(任意のint値)
 
-        jal     fact_start              # factに飛ぶ
+        jal     fact                    # factに飛ぶ
         move    $a0, $v0                # $a0に$v0の値をコピー
 
         li      $v0, 1                  # print_int
+        syscall                         # システムコールの実行
+
+        la      $a0, msg2               # msg2のアドレスを$a0にロード
+        li      $v0, 4                  # print_string
         syscall                         # システムコールの実行
 
         move    $ra, $s0                # mainを呼んだ戻り先のアドレスを$raに代入
@@ -27,16 +31,19 @@ fact:
 
         li      $t0,   1                # $t0に1を代入(分岐命令用)
 
-        bne     $t0, $a0, 2             # $t0と$a0が異なるとき，2つ先の命令へ
-        li      $v0, 0                  # $v0に1を代入
-        j       $ra
-
+        bgeu    $a0, $t0, else          # $t0が$a0より小さいとき，elseへ
+        li      $v0, 1                  # $v0に1を代入
+        lw      $ra, 20($sp)            # 保存しておいた$spを復元
+        lw      $fp, 16($sp)            # 保存しておいた$fpを復元
+        addu    $sp, $sp, 32            # スタックを開放
+        j       $ra                     # 呼び出し元に戻る
+else:   
         addi    $a0, -1                 # $a0から1を引く
         jal     fact                    # 呼び出し元に戻る
 
         lw      $ra, 20($sp)            # 保存しておいた$spを復元
         lw      $fp, 16($sp)            # 保存しておいた$fpを復元
-        lw      $a0, 24($fp)            # 対応する$a0の値を復元
+        lw      $a0, 24($sp)            # 対応する$a0の値を復元
         addu    $sp, $sp, 32            # スタックを開放
 
         mulo    $v0, $a0, $v0           # $v0と$a0をかけて$v0に代入
@@ -46,3 +53,7 @@ fact:
         .align 2
 msg:
         .asciiz "The factorial of 10 is "
+
+        .align 2
+msg2:
+        .asciiz "\n"
