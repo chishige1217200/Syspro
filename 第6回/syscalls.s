7f4ffc8026c827724b4709ba52a820f9848d1f08
chishige1217200 <daichicchi01@gmail.com>
2020-12-07 16:03:56 +0900

    [fix] スタックの積立

--- 第6回/syscalls.s
+++ 第6回/syscalls.s
@@ -3,45 +3,45 @@
 
 # void print(int i)
 _print_int:
-    addiu   $sp,    24          # スタックの積立
+    subu    $sp,    $sp,    24  # スタックの積立
     sw      $ra,    20($sp)     # $raレジスタの値をスタックに退避
 
     li      $v0,    1           # syscall用にprint_intを指定
     syscall                     # システムコールの実行
 
     lw      $ra,    20($sp)     # $raレジスタの値の復元
-    addiu   $ra,    24          # スタックを解放
+    addu    $sp,    $sp,    24  # スタックを解放
     j       $ra                 # 呼び出し元に戻る
 
 _print_string:
-    addiu   $sp,    24          # スタックの積立
+    subu    $sp,    $sp,    24  # スタックの積立
     sw      $ra,    20($sp)     # $raレジスタの値をスタックに退避
 
     li      $v0,    4           # syscall用にprint_stringを指定
     syscall                     # システムコールの実行
 
     lw      $ra,    20($sp)     # $raレジスタの値の復元
-    addiu   $ra,    24          # スタックを解放
+    addu    $sp,    $sp,    24  # スタックを解放
     j       $ra                 # 呼び出し元に戻る
 
 _read_int:
-    addiu   $sp,    24          # スタックの積立
+    subu    $sp,    $sp,    24  # スタックの積立
     sw      $ra,    20($sp)     # $raレジスタの値をスタックに退避
 
     li      $v0,    5           # syscall用にread_intを指定
     syscall                     # システムコールの実行
 
     lw      $ra,    20($sp)     # $raレジスタの値の復元
-    addiu   $ra,    24          # スタックを解放
+    addu    $sp,    $sp,    24  # スタックを解放
     j       $ra                 # 呼び出し元に戻る
 
 _read_string:
-    addiu   $sp,    24          # スタックの積立
+    subu    $sp,    $sp,    24  # スタックの積立
     sw      $ra,    20($sp)     # $raレジスタの値をスタックに退避
 
     li      $v0,    8           # syscall用にread_stringを指定
     syscall                     # システムコールの実行
 
     lw      $ra,    20($sp)     # $raレジスタの値の復元
-    addiu   $ra,    24          # スタックを解放
+    addu    $sp,    $sp,    24  # スタックを解放
     j       $ra                 # 呼び出し元に戻る
